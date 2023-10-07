a942f3ae0141db97e2e13eccc91ef8dd2b0592ac
IceMan0D <jangcom2562@gmail.com>
2023-10-07 17:39:31 +0700

    add print

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("we love heeee");
+        System.out.println("we love heeeeee");
     }
 }
