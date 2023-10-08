ca7a223411dccfd399d5f654b763ec529498dba6
IceMan0D <jangcom2562@gmail.com>
2023-10-08 19:48:34 +0700

    finnish ui

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.testgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_AND_NOTE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("print new");
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//               Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+//               startActivity(intent);            }
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_AND_NOTE
+                );
+            }
+        });
+
     }
 }
