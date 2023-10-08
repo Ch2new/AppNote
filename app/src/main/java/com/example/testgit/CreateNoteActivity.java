54f157857e704a63a24b838dca8cc6fe6dfbaad7
IceMan0D <jangcom2562@gmail.com>
2023-10-08 21:45:05 +0700

    add code to CreateNoteActivity

--- app/src/main/java/com/example/testgit/CreateNoteActivity.java
+++ app/src/main/java/com/example/testgit/CreateNoteActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteTitleSubtitle, inputNote;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,31 @@ public class CreateNoteActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteTitleSubtitle = findViewById(R.id.inputNoteTitleSubtitle);
+        inputNote = findViewById(R.id.inputNote);
+
+        textDateTime = findViewById(R.id.textDateTime);
+
+        //set Date
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd/MM/yyyy Hh:Mm a",Locale.getDefault());
+        textDateTime.setText(sdf.format(new Date()));
+
+
     }
+
+    private void saveNote (){
+        if(inputNoteTitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this,"Note Title can't be empty!", Toast.LENGTH_SHORT).show();
+            return ;
+        }else if(inputNoteTitleSubtitle.getText().toString().trim().isEmpty() && inputNote.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+    }
+
+
+
 }
