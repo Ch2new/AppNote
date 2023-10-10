4da4055fa370a20fcadea34b229da483b698ce10
IceMan0D <jangcom2562@gmail.com>
2023-10-10 18:55:59 +0700

    edit tile and set color background

--- app/src/main/java/com/example/testgit/adapters/NoteAdapters.java
+++ app/src/main/java/com/example/testgit/adapters/NoteAdapters.java
@@ -1,5 +1,7 @@
 package com.example.testgit.adapters;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,6 +56,7 @@ public class NoteAdapters extends RecyclerView.Adapter<NoteAdapters.NoteViewHold
             textTitle = itemView.findViewById(R.id.textTitle);
             textSubtitle = itemView.findViewById(R.id.textSubtitle);
             textDateTime = itemView.findViewById(R.id.textDateTime);
+            layoutNote = itemView.findViewById(R.id.layoutNote);
         }
 
         void setNote(Note note){
@@ -64,7 +67,14 @@ public class NoteAdapters extends RecyclerView.Adapter<NoteAdapters.NoteViewHold
                 textSubtitle.setText(note.getSubtitle());
             }
             textDateTime.setText(note.getDateTime());
-        }
+
+            GradientDrawable gradientDrawable = (GradientDrawable) layoutNote.getBackground();
+            if(note.getColor() != null){
+                gradientDrawable.setColor(Color.parseColor(note.getColor()));
+            }else {
+                gradientDrawable.setColor(Color.parseColor("#333333"));
+            }
+         }
     }
 
 }
